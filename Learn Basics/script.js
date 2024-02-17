420ce671ca855e31cfcde893a9d1e1822d8f0f83
Yvan Nanfack <contact@yvannanfack.com>
2024-02-17 19:09:45 +0100

    create new shapes

--- Learn Basics/script.js
+++ Learn Basics/script.js
@@ -2,20 +2,30 @@
 const scene = new THREE.Scene()
 
 // Cube
-const geometry = new THREE.BoxGeometry(1, 1, 1)
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const torus = new THREE.Mesh(geometry, material)
+torus.rotation.x = 1
+torus.rotation.y = 0.5
+torus.rotation.z = 0.2
+scene.add(torus)
+
+// Capsule Geometry
+const geometry2 = new THREE.CapsuleGeometry( 5, 10, 4, 8 );
+const material2 = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+const capsule = new THREE.Mesh( geometry2, material2 );
+capsule.rotation.z = 1
+scene.add( capsule );
 
 // Sizes
 const sizes = {
-  height: 562.5,
-  width: 1000
+  height: window.visualViewport.height,
+  width: window.visualViewport.width
 }
 
 // Camera
-const camera = new THREE.PerspectiveCamera(45, sizes.width/sizes.height)
-camera.position.z = 3
+const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height)
+camera.position.z = 50
 scene.add(camera)
 
 // Render
