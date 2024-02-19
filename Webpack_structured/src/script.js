9783cd7020c2230765bc65d9c9f124f375ff275e
Yvan Nanfack <contact@yvannanfack.com>
2024-02-19 05:41:24 +0100

    play with camera

--- Webpack_structured/src/script.js
+++ Webpack_structured/src/script.js
@@ -1,5 +1,6 @@
 import "./style.css"
 import gsap from "gsap"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 import {
   Scene,
   TorusGeometry,
@@ -12,11 +13,23 @@ import {
   Clock
 } from "three"
 
+/**
+ * Cursor
+ */
+const cursor = {
+  x: 0,
+  y: 0
+}
+window.addEventListener("mousemove", (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5
+  cursor.y = event.clientY / sizes.height - 0.5
+})
+
 // Scene
 const scene = new Scene()
 
 // Cube Geometry
-const geometry = new TorusGeometry( 10, 3, 16, 100)
+const geometry = new TorusGeometry( 10, 3, 16, 40)
 const material = new MeshBasicMaterial({ color: 0xff00ff, wireframe: true })
 const torus = new Mesh(geometry, material)
 
@@ -36,8 +49,8 @@ torusGroup.add(torus)
 scene.add(torusGroup)
 
 // // Camera
-const camera = new PerspectiveCamera(75, sizes.width/sizes.height)
-camera.position.set(40, 40, 40)
+const camera = new PerspectiveCamera(45, sizes.width/sizes.height)
+camera.position.z = -60
 scene.add(camera)
 
 camera.lookAt(torus.position)
@@ -49,14 +62,17 @@ const renderer = new WebGLRenderer({
 })
 
 renderer.setSize(sizes.width, sizes.height)
-gsap.to(torus.position, { duration: 2, delay: 1, x: 100, y: 50 })
-gsap.to(torus.position, { duration: 2, delay: 2, x: 0, y: 0 })
 
 // Annimation
 let time = new Clock()
 const spin = () => {
+  camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 60
+  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 60
+  camera.position.y = cursor.y * 50
+
+  camera.lookAt(torusGroup.position)
   const elapsedTime = time.getElapsedTime()
-  torusGroup.rotation.z += 0.009 * elapsedTime
+  torusGroup.rotation.z += 0.0001 * elapsedTime
   renderer.render(scene, camera)
   window.requestAnimationFrame(spin)
 }
