9699f1a3c7e66209f23874c8751620d217f8f778
Yvan nanfack <contact@yvannanfack.com>
2024-03-08 04:33:40 +0000

    play with textures

---
+++ Learn-texture/src/assets/door/index.ts
@@ -0,0 +1,17 @@
+import ambientOcclusion from "./ambientOcclusion.jpg"
+import roughnessTexture from "./roughness.jpg"
+import metalnessTexture from "./metalness.jpg"
+import normalTexture from "./normal.jpg"
+import heightTexture from "./height.jpg"
+import alphaTexture from "./alpha.jpg"
+import colorTexture from "./color.jpg"
+
+export default [
+    ambientOcclusion,
+    roughnessTexture,
+    metalnessTexture,
+    normalTexture,
+    heightTexture,
+    alphaTexture,
+    colorTexture
+]
